a9cb0994b8d01cfbe1e480bd64681b9e635747e7
happyfishes <ghbstart@163.com>
2021-04-04 21:34:52 +0800

    自定义list filter

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -19,6 +19,20 @@ class CategoryAdmin(admin.ModelAdmin):
 
     post_count.short_description = '文章数量'
 
+class CategotyOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分裂 """
+    title = '分类过滤器'
+    parameter_name = 'owner_categoty'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -33,10 +47,11 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
-        'title', 'category', 'status', 'created_time', 'operator'
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
     ]
     list_display_links = []
-    list_filter = ['category', ]
+    list_filter = [CategotyOwnerFilter, ]
     search_fields = ['title', 'category__name']
     # save_on_top = True
 
@@ -64,3 +79,7 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
