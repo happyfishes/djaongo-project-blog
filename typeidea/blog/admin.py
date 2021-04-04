a2a3375070478006171d192b55605b8b0fa8245a
happyfishes <ghbstart@163.com>
2021-04-04 18:31:25 +0800

    增加admin配置

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
