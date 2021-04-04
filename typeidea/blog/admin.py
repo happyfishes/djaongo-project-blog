136cd4fcf0f3aabe5cdfb79556f74b3e0c4cbe22
happyfishes <ghbstart@163.com>
2021-04-04 22:49:10 +0800

    blog\admin.py修改

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -3,21 +3,22 @@ from django.contrib.admin.models import LogEntry
 from django.urls import reverse
 from django.utils.html import format_html
 
-from .adminform import PostAdminForm
 
+from .adminform import PostAdminForm
 from .models import Post, Category, Tag
 from typeidea.custom_site import custom_site
 from typeidea.base_admin import BaseOwnerAdmin
 
 
-class PostInline(admin.TabularInline):
+class PostInline(admin.TabularInline):  # StackedInline  样式不同
     fields = ('title', 'desc')
-    extra = 1
+    extra = 1  # 控制额外多几个
     model = Post
 
 
 @admin.register(Category, site=custom_site)
 class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline, ]
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
@@ -27,10 +28,17 @@ class CategoryAdmin(BaseOwnerAdmin):
     post_count.short_description = '文章数量'
 
 
-class CategotyOwnerFilter(admin.SimpleListFilter):
-    """ 自定义过滤器只展示当前用户分裂 """
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分类 """
+
     title = '分类过滤器'
-    parameter_name = 'owner_categoty'
+    parameter_name = 'owner_category'
 
     def lookups(self, request, model_admin):
         return Category.objects.filter(owner=request.user).values_list('id', 'name')
@@ -42,12 +50,6 @@ class CategotyOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Tag, site=custom_site)
-class TagAdmin(BaseOwnerAdmin):
-    list_display = ('name', 'status', 'created_time')
-    fields = ('name', 'status')
-
-
 @admin.register(Post, site=custom_site)
 class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
@@ -56,7 +58,8 @@ class PostAdmin(BaseOwnerAdmin):
         'created_time', 'owner', 'operator'
     ]
     list_display_links = []
-    list_filter = [CategotyOwnerFilter, ]
+
+    list_filter = [CategoryOwnerFilter, ]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
@@ -64,9 +67,8 @@ class PostAdmin(BaseOwnerAdmin):
 
     # 编辑页面
     save_on_top = True
-    # 指定哪些字段不显示
-    exclude = ['owner']
 
+    exclude = ['owner']
     """
     fields = (
         ('category', 'title'),
@@ -92,24 +94,24 @@ class PostAdmin(BaseOwnerAdmin):
         }),
         ('额外信息', {
             'classes': ('wide',),
-            'fields': ('tag',),
+            'fields': ('tag', ),
         })
     )
     # filter_horizontal = ('tag', )
-    filter_vertical = ('tag',)
+    # filter_vertical = ('tag', )
 
     def operator(self, obj):
         return format_html(
-            '<a href="{}">编辑</a>', reverse('cus_admin:blog_post_change', args=(obj.id,))
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
-
     operator.short_description = '操作'
 
     class Media:
         css = {
-            'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
+            'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
         }
-        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js', )
 
 
 @admin.register(LogEntry, site=custom_site)
