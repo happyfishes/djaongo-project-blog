c21cb361548b950fa4ab01ec562b0c67c84cd321
happyfishes <ghbstart@163.com>
2021-04-04 20:34:10 +0800

    保存category和tag是自动绑定当前用户

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -4,14 +4,19 @@ from .models import Post, Category, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
 
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
